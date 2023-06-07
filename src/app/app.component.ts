15602d2c5ce85cd459f63c2f1a1ddb4be57ebed8
Andrew Hall <andrewmartinhall2@gmail.com>
2023-06-07 11:50:33 -0600

    4. Visualizing Tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Task } from './task/task';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kanban-fire';
+  todo: Task[] = [
+  {title: 'Buy milk',
+    description: 'Go to the store and buy milk'
+  },
+  {
+    title: 'Create a Kanban app',
+    description: 'Using Firebase and Angular create a Kanban app!'
+  },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //Material Modules
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { TaskComponent } from './task/task.component';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
