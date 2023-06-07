d3639970f58e362370e75215fbef8834e77b0d1b
Andrew Hall <andrewmartinhall2@gmail.com>
2023-06-07 12:23:00 -0600

    10. Adding Firebase to the Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,17 @@ import { MatInputModule } from '@angular/material/input';
 //Components
 import { TaskComponent } from './task/task.component';
 import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -36,8 +47,20 @@ import { TaskDialogComponent } from './task-dialog/task-dialog.component';
     MatDialogModule,
     FormsModule,
     MatInputModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage()),
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyA28MkX_PUnfHsKQXBSPEZKSDUwfy4jOD8",
+    authDomain: "kanban-fire-7edb2.firebaseapp.com",
+    projectId: "kanban-fire-7edb2",
+    storageBucket: "kanban-fire-7edb2.appspot.com",
+    messagingSenderId: "141452812716",
+    appId: "1:141452812716:web:0929e72210e6950e859a09"
+  },
+};
